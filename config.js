a6584fd0594a270661a66895d4ebb0822fa4c95e
abdullahsiddiqui9999 <abdullah.siddiqui9999@gmail.com>
2021-05-23 17:16:59 +0500

    Facebook passport

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/conFusion",
+  facebook: {
+    clientId: "501349017675173",
+    clientSecret: "54029825e83e8683fadd8a11572e8373",
+  },
 };
